85d0ed3b49a09721dc922aba6b66fc83984d5878
coderjibon <contact.engjibon@gmail.com>
2022-01-16 20:19:38 +0600

    status code

---
+++ index.js
@@ -0,0 +1,30 @@
+//Library include
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan"); // log generator
+const PORT = process.env.PORT;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+
+//route
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        "status": true,
+        "message":"Amazon clone REST API Home page."
+    });
+});
+
+
+
+
+
+//server start
+app.listen(PORT, () => {
+    console.log(`Server running at port: ${PORT}`);
+});
